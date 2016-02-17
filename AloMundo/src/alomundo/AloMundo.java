0492caa4b3b998ac798249488c8fe1dd0a1842fe
GlobalIdéias <globalideiasml@gmail.com>
2016-02-17 14:45:04 -0300

    Atualização 2

---
+++ AloMundo/src/alomundo/AloMundo.java
@@ -0,0 +1,20 @@
+
+package alomundo;
+
+/**
+ *
+ * @author globalideias
+ */
+public class AloMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Ola mundo....");
+        
+        
+    }
+    
+}
